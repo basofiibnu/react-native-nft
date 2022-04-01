b4f2c6783ad018e313995705859e7c1a15a5e3cf
basofiibnu <>
2022-04-02 00:02:18 +0700

    first commit and init base components

---
+++ components/index.js
@@ -0,0 +1,12 @@
+import HomeHeader from './HomeHeader';
+import FocusedStatusBar from './FocusedStatusBar';
+import NFTCard from './NFTCard';
+import { CircleButton, RectButton } from './button';
+
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+};
